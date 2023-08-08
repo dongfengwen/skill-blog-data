6a21f0889919032355a4567bc88266dd829163e7
董凤文Z1003 <fengwen.dong@going-link.com>
2023-08-08 16:10:47 +0800

    [init]初始化

---
+++ dskill.blog/main/java/com/blog/SkillBlogApplication.java
@@ -0,0 +1,16 @@
+package com.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: fengwen.dong@going-link.com
+ * @createDate: 2023-01-03 16:34
+ */
+@SpringBootApplication
+public class SkillBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SkillBlogApplication.class, args);
+    }
+}
